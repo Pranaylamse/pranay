53d169fc5bd0146ed9300410035ad55d032fda6a
pranay lamse <pranaylamsegood@gmail.com>
2020-10-10 20:06:17 +0530

    Updates

---
+++ precache-manifest.a8718cf673372a7d6f7f721ab31fafd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856eaca6b2f934580f13109f2dfb48c5",
+    "url": "/pranay/index.html"
+  },
+  {
+    "revision": "ac17753c2048f3450028",
+    "url": "/pranay/static/css/main.88df9845.chunk.css"
+  },
+  {
+    "revision": "1a7ca46c6a2cf678a04d",
+    "url": "/pranay/static/js/2.36b6ca63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pranay/static/js/2.36b6ca63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac17753c2048f3450028",
+    "url": "/pranay/static/js/main.a3fe71f5.chunk.js"
+  },
+  {
+    "revision": "86c58a32cc6510fd05b3",
+    "url": "/pranay/static/js/runtime-main.768846bc.js"
+  },
+  {
+    "revision": "4eed725715f1ba4926cc4bc9a43fbd1a",
+    "url": "/pranay/static/media/Photo.4eed7257.jpeg"
+  }
+]);
